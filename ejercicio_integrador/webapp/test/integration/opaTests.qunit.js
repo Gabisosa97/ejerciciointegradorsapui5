e9a21fa7067139f4e07a32e8f23630ee41f5c581
Gabriel Sosa <gabisosa97@gmail.com>
2023-09-27 17:53:08 +0000

    + Deploy

--- ejercicio_integrador/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["acc/int/ejerciciointegrador/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- ejercicio_integrador/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"accint/ejercicio_integrador/test/unit/controller/Main.controller"
-], function () {
-	"use strict";
-});
